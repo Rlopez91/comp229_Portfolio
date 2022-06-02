541a69d6e54c4f7d25963867dcfc834133c24b1b
Rick L <richyray24@gmail.com>
2022-06-02 12:32:37 -0400

    created projects page and some custom css

--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -15,7 +15,7 @@ router.get('/about', function (req, res, next) {
     res.render('index', { title: 'About Me', page: 'about' });
 });
 router.get('/projects', function (req, res, next) {
-    res.render('index', { title: 'Me Projects', page: 'projects' });
+    res.render('index', { title: 'My Projects', page: 'projects' });
 });
 router.get('/services', function (req, res, next) {
     res.render('index', { title: 'My Services', page: 'services' });
