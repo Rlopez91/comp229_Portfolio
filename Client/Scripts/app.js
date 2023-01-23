8edc20ec60a121aecbe6cb888b48cf022f0f8d24
Rick L <richyray24@gmail.com>
2023-01-22 21:57:01 -0500

    upgrading yarn version

--- Client/Scripts/app.js
+++ Client/Scripts/app.js
@@ -1,15 +1,9 @@
-/*
-COMP229 Express Portfolio
-Ricardo Lopez
-Student ID: 301167302
-June 2, 2022
-*/
 "use strict";
 (function () {
     function Start() {
         console.log("App Started!");
         let XHR = new XMLHttpRequest();
-        XHR.open("GET", "../data/data.json");
+        XHR.open("GET", "../Data/data.json");
         XHR.send();
         XHR.addEventListener("readystatechange", function () {
             if (XHR.readyState == 4 && XHR.status == 200) {
@@ -21,12 +15,4 @@ June 2, 2022
     }
     window.addEventListener("load", Start);
 })();
-
-
-//submit button on click event and alert
-document.getElementById("redirect").onclick = function(){
-    console.log("it is working");
-    confirm("You will be redirected to Home Page");
-    location.href = "localhost:3000/home";
-};
 //# sourceMappingURL=app.js.map
